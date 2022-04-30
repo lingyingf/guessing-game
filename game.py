9f3b663cb4d23472028160799888f907cb471a63
Anita <lingyingf61422@gmail.com>
2022-04-30 12:14:58 -0700

    Game play!

--- game.py
+++ game.py
@@ -10,7 +10,29 @@ corret number is guessed. guessed number == random number --> break loop
 when a guess happens, we will += 1 to the count_guess variable
 4) When loop ends, print - good job {name} you guessed in {count_guess} tries!
 '''
+import random
 
 print("Hello! What is your name? ")
 name = input("> ")
-print("Hi", name, "welcome to the game!")
+print("Hi", name, "welcome to the game!Please add a number between 1 - 100")
+guessed_number = int(input("> "))
+random_number = random.randint(1,101)
+
+def guess(guessed_number):
+    guess_count = 1
+    while guessed_number != random_number:
+        if guessed_number > random_number:
+            print("Too high. Guess again.")
+            guessed_number = int(input("> "))
+            guess_count += 1
+        elif guessed_number < random_number:
+            print("Too low. Guess again.")
+            guessed_number = int(input("> "))
+            guess_count += 1
+
+    print(f"You win! You guessed the correct number in {guess_count} guesses!")
+
+
+guess(guessed_number)
+    
+            
